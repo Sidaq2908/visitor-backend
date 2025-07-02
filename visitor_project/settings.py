50ea9a70bd1071c23d9ce74e5d7dc15deb5003c6
Sidaq <sidqu29801@gmail.com>
2025-07-02 15:09:16 +0530

    Changes in settings.py

--- visitor_project/settings.py
+++ visitor_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-diahpl+^j!_u8@qu^%+7m1x(qhavey*(8w%_)l)rjq!w$@ix_%'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','your-default-dev-secret')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG','')!='False'
 
 ALLOWED_HOSTS = ['*']
 
@@ -50,7 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
+    'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
@@ -79,14 +81,10 @@ WSGI_APPLICATION = 'visitor_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': "ducatprojdb",
-        'USER' : "root",
-        'PASSWORD' : "root",
-        'HOST':'localhost',
-        'PORT': '3306',
-    }
+    'default': dj_database_url.config(
+       default=os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3'),
+       conn_max_age=600,
+    )
 }
 
 
@@ -126,6 +124,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
